082da7bbd9b29c42de282d9d50194ed6ca57be64
imjayyy <54414096+imjayyy@users.noreply.github.com>
2022-11-24 01:38:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ class Config(object):
     DEBUG = True
     TESTING = False
     SECRET_KEY = "1235asdzxqer34asd31sdf"
-    MAIL_SERVER = 'mail.ehelpingbusiness.com'
+    MAIL_SERVER = 'mail.xxxxxxxxxx.com'
     MAIL_PORT= 587
-    MAIL_USERNAME = 'hrm@ehelpingbusiness.com'
-    MAIL_PASSWORD = 'pass@hrm.com'
+    MAIL_USERNAME = 'abc@xxxxxxxxxx.com'
+    MAIL_PASSWORD = 'pass@abc.com'
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
 
